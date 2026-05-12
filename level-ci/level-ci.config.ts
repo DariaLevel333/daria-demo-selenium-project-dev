dee0674a7cce78441efa66e4e75a84a5bc468d48
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2026-05-12 16:01:56 +0300

    Update config

--- level-ci/level-ci.config.ts
+++ level-ci/level-ci.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@level-ci/cli";
 export default {
-  organization: "ivan-haliurov-1-userway-org",
-  project: "a11y-selenium-java-sample",
+  organization: "ivan-haliurov-levelaccess-com-hspeq",
+  project: "demo-selenium-project",
   token: process.env.LEVEL_CI_TOKEN,
   server: "https://api.dev.userway.dev",
   reportPaths: ["./level-ci-reports"],
