008fb8604e1ca9aedcd8c15c5a6293fc8bbebb79
Daria <daria.osokina@levelaccess.com>
2026-05-13 17:38:44 +0300

    initial commit

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@level-ci/cli";
 export default {
-  organization: "ivan-haliurov-levelaccess-com-hspeq",
-  project: "demo-selenium-project",
+  organization: "daria-osokina-1-levelaccess-com-kofcj",
+  project: "test-website-1-mirko-github-daria",
   token: process.env.LEVEL_CI_TOKEN,
   server: "https://api.dev.userway.dev",
   reportPaths: ["./level-ci/level-ci-reports"],
